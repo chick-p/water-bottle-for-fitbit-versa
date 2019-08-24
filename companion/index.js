e78741dc7b0b50d904ea40b3ce9c81f003db0fd8
chick-p <chick.p.1120@gmail.com>
2019-08-24 11:00:39 +0900

    Improved: eslint

--- companion/index.js
+++ companion/index.js
@@ -1,36 +1,36 @@
-import * as messaging from "messaging";
-import { settingsStorage } from "settings";
+import * as messaging from 'messaging';
+import { settingsStorage } from 'settings';
 
-settingsStorage.setItem(
-  "meditate",
-  JSON.stringify({ selected: [0], values: [{ name: "5 minites", value: 5 }] })
-);
+const sendVal = (data) => {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  }
+}
 
-messaging.peerSocket.onmessage = evt => {};
-
-messaging.peerSocket.onopen = () => {
-  restoreSettings();
-};
-
-messaging.peerSocket.onerror = err => {
-  console.log("Connection error: " + err.code + " - " + err.message);
-};
-
-settingsStorage.onchange = evt => {
-  sendVal({ key: evt.key, newValue: evt.newValue });
-};
-
-function restoreSettings() {
+const restoreSettings = () => {
   for (let index = 0; index < settingsStorage.length; index++) {
-    let key = settingsStorage.key(index);
+    const key = settingsStorage.key(index);
     if (key) {
       sendVal({ key: key, newValue: settingsStorage.getItem(key) });
     }
   }
 }
 
-function sendVal(data) {
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send(data);
-  }
-}
+settingsStorage.setItem(
+  'meditate',
+  JSON.stringify({ selected: [0], values: [{ name: '5 minites', value: 5 }] })
+);
+
+messaging.peerSocket.onmessage = () => {};
+
+messaging.peerSocket.onopen = () => {
+  restoreSettings();
+};
+
+messaging.peerSocket.onerror = (err) => {
+  console.log('Connection error: ' + err.code + ' - ' + err.message);
+};
+
+settingsStorage.onchange = (evt) => {
+  sendVal({ key: evt.key, newValue: evt.newValue });
+};
--- settings/index.jsx
+++ settings/index.jsx
@@ -3,15 +3,14 @@ function mySettings(props) {
     <Page>
       <Section
         title={<Text bold align="center">Settings</Text>}>
-
       <Select
         label={`Meditate`}
         settingsKey="meditate"
         options={[
-          {name:"5 minites", value:5},
-          {name:"10 minites", value:10},
-          {name:"15 minites", value: 15},
-          {name:"20 minites", value: 20}
+          { name: "5 minites", value: 5 },
+          { name: "10 minites", value: 10 },
+          { name: "15 minites", value: 15 },
+          { name: "20 minites", value: 20 }
         ]}
       />
       </Section>
